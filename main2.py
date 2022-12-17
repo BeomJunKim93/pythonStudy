6c2ee9827e6371ac03ab603afcf5453f4fb4a4d5
BeomJunKim93 <bumji21@hanyang.ac.kr>
2022-12-17 16:41:26 +0900

    221217 파일 합치기 실습

--- main2.py
+++ main2.py
@@ -1,6 +1,7 @@
-import pandas as pd
-import numpy as np
-import pyexcel
+# import pandas as pd
+# import numpy as np
+# import pyexcel
+# "D:/Dropbox/Dropbox/Hongeun119/data/파일명.xls"
 
 # def read_data(dir):
 #     f = open(dir, 'r')
@@ -17,7 +18,40 @@ import pyexcel
 # f = open("./새파일2.csv", 'w')
 # f.writelines(res)
 # f.close()
-
-a = 4
-b = 6
+#
+# a = 4
+# b = 6
+'''
 print(a+b)
+'''
+
+"""
+a + b
+"""
+
+number = 3
+day = 'three'
+a = "I eat %d apples in %s days." % (number, day)
+print(a)
+
+
+import os
+
+file_list = os.listdir('D:/Dropbox/Dropbox/Hongeun119/data')
+print(file_list)
+
+def read_data(dir):
+    f = open(dir, 'r')
+    data = f.readlines()
+    f.close()
+    return data
+
+data = read_data("D:/Dropbox/Dropbox/Hongeun119/data/%s" % file_list[5])
+data2 = read_data("D:/Dropbox/Dropbox/Hongeun119/data/%s" % file_list[6])
+
+res = data + data2[1:]
+print(res)
+
+f = open("D:/Dropbox/Dropbox/Hongeun119/data/취합1.xls", 'w')
+f.writelines(res)
+f.close()
